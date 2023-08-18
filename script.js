4a441219db796876d7888f3471250ac64a81274b
kittu246 <119164660+kittu246@users.noreply.github.com>
2023-08-18 10:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //your JS code here. If required.
+#main-divsn{
+    background-color: rgb(249, 248, 248);
+    width:500px;
+    padding:60px;
+    box-shadow: 15px 15px 15px grey;
+    text-align: center;
+    height: auto;
+}
+#pic{
+
+    
+    width: 400px;
+    height: 300px;
+    
+}
+h1{
+    margin-top: 0px;
+   font-weight: bold;
+   font-size: 70px;
+   margin-bottom: 0px;
+}
+p{
+    margin-top: 0px;
+    font-weight: bold;
+    font-size: 35px;
+}
+#btn{
+  background-color: rgb(182, 79, 53);
+  border-radius: 10px;
+  color: white;
+  border: none;
+  padding:20px;
+  font-size: 40px;
+
+}
